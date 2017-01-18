8debd01f791336a3f907f348525ac536c00f09b5
Eric Wennerberg <ericwenn@gmail.com>
2017-01-18 14:30:32 +0100

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -16,7 +16,7 @@ public class Main {
 			Integer train2_speed = Integer.parseInt(args[2]);
 			Integer tsim_speed = (args.length >= 4) ? Integer.parseInt(args[3]) : 20;
 			
-			String tsimCommand = String.format("/chalmers/groups/tda381/bin/tsim --speed=%d %s", tsim_speed, map);
+			String tsimCommand = String.format("/usr/local/bin/tsim --speed=%d %s", tsim_speed, map);
 			Process p = Runtime.getRuntime().exec(tsimCommand);
 			TSimInterface.init(p.getInputStream(), p.getOutputStream());
 			TSimInterface.getInstance().setDebug(true);
